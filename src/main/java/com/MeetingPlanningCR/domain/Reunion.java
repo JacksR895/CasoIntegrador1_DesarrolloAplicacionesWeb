ff916bc8d6139e4cf73accc37053e813e6af5c59
Isaac Hernandez <isaac.f.hernandez@outlook.com>
2026-02-24 19:39:40 -0600

    Added all constructors for entities

--- src/main/java/com/MeetingPlanningCR/domain/Reunion.java
+++ src/main/java/com/MeetingPlanningCR/domain/Reunion.java
@@ -1,4 +1,55 @@
 package com.MeetingPlanningCR.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
 public class Reunion {
+
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotNull
+    private LocalDate fecha;
+
+    @NotBlank
+    private String hora;
+
+    @NotNull
+    private Long salaId;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public String getHora() {
+        return hora;
+    }
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    public Long getSalaId() {
+        return salaId;
+    }
+    public void setSalaId(Long salaId) {
+        this.salaId = salaId;
+    }
+
 }
--- src/main/java/com/MeetingPlanningCR/domain/Sala.java
+++ src/main/java/com/MeetingPlanningCR/domain/Sala.java
@@ -1,4 +1,36 @@
 package com.MeetingPlanningCR.domain;
 
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class Sala {
+
+    private Long id;
+
+    @NotBlank
+    private String nombre;
+
+    @Min(1)
+    private int capacidad;
+
+    private Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
 }
